1ba6e1befeec38e92799325f62183cd0e0cd153f
Ronald Elzen <ronald.elzen@student.stenden.com>
2018-05-08 11:13:48 +0200

    First version of navigation

---
+++ Wolfpack/Wolfpack.Web/Scripts/main.js
@@ -0,0 +1,32 @@
+﻿let isCollapsed = false;
+
+document.querySelector(".toggle-menu").addEventListener('click', function () {
+    document.querySelector(".sidebar").style.marginLeft = "0"
+    let overlay = document.createElement('div');
+    overlay.classList.add("sidebar-overlay");
+    overlay.addEventListener('click', function () {
+
+        document.querySelector(".sidebar").style.marginLeft = "-200px";
+        overlay.remove();
+    });
+    document.querySelector(".wrapper").appendChild(overlay);
+});
+
+
+
+if (window.innerWidth <= 1024 && window.innerWidth >= 600) {
+    document.querySelector(".sidebar").classList.add("collapsed");
+    isCollapsed = true;
+}
+
+document.querySelector("#toggle-collapse").addEventListener('click', function () {
+    if (isCollapsed) {
+        document.querySelector(".sidebar").classList.remove("collapsed");
+        isCollapsed = false;
+
+    }
+    else {
+        document.querySelector(".sidebar").classList.add("collapsed");
+        isCollapsed = true;
+    }
+});
