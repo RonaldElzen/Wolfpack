47bd250e0944bb0cc21497cc7e4336360ce818ae
Mart Hidding <Marthidding@gmail.com>
2018-05-08 11:04:03 +0200

    Created Create new user page

---
+++ Wolfpack/Wolfpack.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wolfpack.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewUser()
+        {
+            ViewBag.Message = "Create a new account.";
+
+            return View();
+        }
+
+    }
+}
