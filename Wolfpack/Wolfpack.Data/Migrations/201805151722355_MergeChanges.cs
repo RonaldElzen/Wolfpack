14916e8e6ba6bde8abbe513ca6d3417edd275f41
Raymond Benjamins <raymond.benjamins@telfort.nl>
2018-05-15 19:25:08 +0200

    Added empty migration to fix Entity Framework thinking there are changes while there are non.

---
+++ Wolfpack/Wolfpack.Data/Migrations/201805151722355_MergeChanges.cs
@@ -0,0 +1,16 @@
+namespace Wolfpack.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MergeChanges : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
