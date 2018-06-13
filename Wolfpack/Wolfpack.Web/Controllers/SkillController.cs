154b434b06dce4eba49748ab2ab33d84bcdd6634
Ronald Elzen <ronald.elzen@student.stenden.com>
2018-06-13 10:51:38 +0200

    Fix for SkillController

--- Wolfpack/Wolfpack.Web/Controllers/SkillController.cs
+++ Wolfpack/Wolfpack.Web/Controllers/SkillController.cs
@@ -2,12 +2,14 @@
 using System.Linq;
 using System.Web.Mvc;
 using Wolfpack.Data;
+using Wolfpack.Web.Helpers.Interfaces;
 
 namespace Wolfpack.Web.Controllers
 {
     public class SkillController : BaseController
     {
-        public SkillController(Context context) : base(context) { }
+        public SkillController(Context context, IUserHelper userHelper = null, ISessionHelper sessionHelper = null) 
+            : base(context, null, null) { }
 
         /// <summary>
         /// Skills in json format for autocomplete
