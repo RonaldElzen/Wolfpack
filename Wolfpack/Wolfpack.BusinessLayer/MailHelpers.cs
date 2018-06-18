a816a14f44e5b3d08311a1a0acfe4f2f08c54e8e
Daan Broere <broeredaan@users.noreply.github.com>
2018-06-18 11:42:59 +0200

    Remove incorrect comment

--- Wolfpack/Wolfpack.BusinessLayer/MailHelpers.cs
+++ Wolfpack/Wolfpack.BusinessLayer/MailHelpers.cs
@@ -7,14 +7,11 @@ using System.Threading.Tasks;
 
 namespace Wolfpack.BusinessLayer
 {
-    //Global helpers class for methods that are needed if multiple places.
     public static class MailHelpers
     {
         /// <summary>
         /// Checks if the email provided is a valid email and returns a boolean
         /// </summary>
-        /// <param name="email"></param>
-        /// <returns></returns>
         public static bool CheckIfValidEmail(string email)
         {
             string pattern = @"^[a-z][a-z|0-9|]*([_][a-z|0-9]+)*([.][a-z|0-9]+([_][a-z|0-9]+)*)?@[a-z][a-z|0-9|]*\.([a-z][a-z|0-9]*(\.[a-z][a-z|0-9]*)?)$";
